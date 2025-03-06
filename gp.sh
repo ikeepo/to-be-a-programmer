9f04ae0f43722afe1e388bef84eeae70b5e08822
ikeepo <ikeepo@proton.me>
2025-03-06 14:16:31 +0800

    u'

--- gp.sh
+++ gp.sh
@@ -6,21 +6,21 @@ remote_name=$(git remote)
 # 获取远程仓库 URL（可选，用于调试或验证）
 remote_url=$(git remote get-url $remote_name)
 
-# 获取本机电脑型号（适用于 Windows 和 Linux）
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    # Linux 系统
-    computer_model=$(hostnamectl | grep "Model" | awk -F: '{print $2}' | xargs | cut -c 1-10)
-    if [ -z "$computer_model" ]; then
-        # 如果hostnamectl命令不可用，可以尝试使用 lscpu 命令
-        computer_model=$(lscpu | grep "Model name" | awk -F: '{print $2}' | xargs | cut -c 1-10)
-    fi
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    # macOS 系统
-    computer_model=$(system_profiler SPHardwareDataType | grep "Model Name" | awk -F: '{print $2}' | xargs)
-else
-    # Windows 系统
-    computer_model=$(wmic computersystem get model | sed -n '2p' | cut -c 1-10)
-fi
+# # 获取本机电脑型号（适用于 Windows 和 Linux）
+# if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+#     # Linux 系统
+#     computer_model=$(hostnamectl | grep "Model" | awk -F: '{print $2}' | xargs | cut -c 1-10)
+#     if [ -z "$computer_model" ]; then
+#         # 如果hostnamectl命令不可用，可以尝试使用 lscpu 命令
+#         computer_model=$(lscpu | grep "Model name" | awk -F: '{print $2}' | xargs | cut -c 1-10)
+#     fi
+# elif [[ "$OSTYPE" == "darwin"* ]]; then
+#     # macOS 系统
+#     computer_model=$(system_profiler SPHardwareDataType | grep "Model Name" | awk -F: '{print $2}' | xargs)
+# else
+#     # Windows 系统
+#     computer_model=$(wmic computersystem get model | sed -n '2p' | cut -c 1-10)
+# fi
 
 # 弹出输入框让用户手动输入提交消息
 echo "请输入提交消息："
